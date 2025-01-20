a20e89a13f2c2129799511a8b5e58f5429e58e7d
Lourdes <lourdes.jupor>
2025-01-20 13:08:07 +0100

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 
-// https://astro.build/config
 export default defineConfig({
-	// site: 'http://localhost:3000/',
-	// base: '/',
-	
+    // site: 'http://localhost:3000/',
+    // base: '/',
+    integrations: [tailwind()],
 });
+
+
