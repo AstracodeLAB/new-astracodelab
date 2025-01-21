a43886ca3dafab7a20d1cb72fa9c9711e174e9fc
Lourdes <lourdes.jupor>
2025-01-21 15:39:47 +0100

    Add site in astro config for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
-
 import sitemap from '@astrojs/sitemap';
 
 
 export default defineConfig({
-    // site: 'http://localhost:3000/',
-    // base: '/',
-    integrations: [tailwind(), sitemap()],
+  site:'https://astracodelab.com/',
+  integrations: [tailwind(), sitemap()],
 });
