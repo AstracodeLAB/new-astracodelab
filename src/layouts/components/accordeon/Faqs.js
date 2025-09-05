5e3a1858166f263c671c0a271fe4d07eee50ee10
Aidablaya <aidablaya@gmail.com>
2025-09-05 16:29:54 +0200

    finish page ecommerce

---
+++ src/layouts/components/accordeon/Faqs.js
@@ -0,0 +1,20 @@
+
+
+export const faqsEcommerce = [
+  {
+    titulo: "¿La tienda online estará adaptada para móviles y tablets?",
+    contenido: "Si. Tu ecommerce se verá y funcionará perfectamente en cualquier dispositivo (ordenadores, tablets y smartphones)."
+  },
+  {
+    titulo: "¿Cuántos productos puedo subir y gestionar yo mismo en mi e-commerce? ¿Me enseñaréis a hacerlo?",
+    contenido: "Podrás incluir <b>productos ilimitados</b> en tu <b>tienda online</b>. Al finalizar el proyecto, te daremos un manual y una <b>formación básica</b> para que puedas gestionar tu inventario, precios, descripciones y pedidos de forma autónoma. Si prefieres delegar esta tarea, nuestro equipo puede encargarse de la gestión del contenido por ti."
+  },
+  {
+    titulo: "¿El servicio de e-commerce incluye dominio y hosting?",
+    contenido: "Sí, nuestros servicios incluyen la gestión inicial y configuración de tu dominio y hosting. "
+  },
+  {
+    titulo: "¿Ofrecéis mantenimiento continuo para la tienda online una vez esté publicada?",
+    contenido: "Sí. Una vez lanzada tu <b>tienda online</b>, el <b> mantenimiento web</b> es fundamental para su éxito a largo plazo. AstracodeLAB, ofrece <b>planes de mantenimiento para e-commerce</b>. Puedes consultar nuestros planes aquí [enlazar a la página de mantenimiento]."
+  },
+];
