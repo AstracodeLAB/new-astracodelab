6cf05678d26a36762029a5965c8f4a3874253610
Aidablaya <aidablaya@gmail.com>
2025-09-05 17:03:06 +0200

    adjust faqs

--- src/layouts/components/accordeon/Faqs.js
+++ src/layouts/components/accordeon/Faqs.js
@@ -1,4 +1,49 @@
-
+export const faqsHome = [
+  {
+    titulo: "¿Qué diferencia hay entre “web corporativa” y “landing page”?",
+    contenido: "Una <b>web corporativa</b> es un sitio completo para reforzar la imagen de tu marca, con secciones como \"Quiénes somos\", \"Servicios\" o \"Contacto\" y está optimizada para tener un diseño web corporativo profesional. Una <b>landing page</b>, en cambio, es para campañas específicas, enfocada en una acción concreta y un mensaje muy directo."
+  },
+  {
+    titulo: "¿Ofrece AstracodeLAB soporte y mantenimiento web después de la entrega?",
+    contenido: "Sí. Disponemos de planes de mantenimiento web, puedes consultarlos desde nuestra sección de<a href='/servicios/mantenimiento'> Mantenimiento y Soporte.</a> "
+  },
+  {
+    titulo: "¿Mi página web se verá bien en móviles y otros dispositivos?",
+    contenido: "Por supuesto, la <b>página web</b> estará adaptada para que se vea correctamente en móviles, tablets y ordenadores (diseño responsive) "
+  },
+  {
+    titulo: "¿Cuántos productos puedo subir y gestionar yo mismo? ¿Me enseñaréis?",
+    contenido: "Podrás incluir productos ilimitados. Al finalizar el proyecto te proporcionaremos un manual para que puedas gestionar tu web. Si por el contrario prefieres delegar esta tarea, nuestro equipo puede encargarse de hacer esta gestión por ti."
+  },
+  {
+    titulo: "¿Cuál es la diferencia entre un rediseño y una web nueva?",
+    contenido: "Un <b>rediseño web</b> reaprovecha la estructura y contenidos existentes, focalizándose en renovar la estética y la experiencia de usuario. Una <b>web nueva</b> implica la creación desde cero de todo el sitio."
+  },
+  {
+    titulo: "Podéis rediseñar solo el logotipo",
+    contenido: "Sí, podemos rediseñar únicamente el logotipo. Nos adaptamos siempre a tus necesidades."
+  },
+  {
+    titulo: "¿Quién escribirá los textos de la web? ¿Me debo encargar de enviaros las imágenes?",
+    contenido: "Lo ideal es que nos facilites tus propios textos e imágenes para reflejar al máximo la esencia de tu marca. No obstante, si lo prefieres, nuestro equipo puede encargarse de redactar los textos."
+  },
+  {
+    titulo: "¿El precio incluye IVA?",
+    contenido: "En la página web se muestran los precios sin IVA para facilitar la comparación de precios. En la factura final se aplicará el 21% de IVA según lo establecido por la legislación española."
+  },
+  {
+    titulo: "¿Seré el propietario de mi web?",
+    contenido: "Absolutamente. Al completar el pago, eres propietario de tu <b>página web</b>, sin licencias ocultas ni permanencias."
+  },
+  {
+    titulo: "¿Qué tipo de webs hacemos?",
+    contenido: "En AstracodeLAB desarrollamos soluciones digitales adaptadas a la meta de cada negocio. Nos especializamos en: <b>webs corporativas, webs a medida, portfolios profesionales, tiendas online (e-commerce), y rediseño y renovación web</b>. Para más detalles, visita nuestra sección de <a href='/servicios/diseno-web'> Diseño y Desarrollo Web.</a>"
+  },
+  {
+    titulo: "¿Dónde presta sus servicios AstracodeLAB?",
+    contenido: "AstracodeLAB está ubicada en Barcelona, Castellón y Málaga, pero presta servicios para toda España."
+  },
+]
 
 export const faqsEcommerce = [
   {
